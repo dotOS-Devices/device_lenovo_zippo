8fe98ef27377c5ebef1bffe40a31a4edffbae142
wilmabumsson <pete80pro.pb@gmail.com>
2021-03-26 14:28:26 +0100

    zippo: Change FP to Redfin August
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Id1cca841d0a90c8251a0b6f3d2148ebb4c74dec4

--- derp_zippo.mk
+++ derp_zippo.mk
@@ -23,10 +23,10 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z6 Pro
 PRODUCT_NAME := derp_zippo
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
     PRODUCT_NAME="zippo" \
     TARGET_DEVICE="zippo"
 
